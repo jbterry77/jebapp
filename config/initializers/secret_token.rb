d87690ecff9f75af4121741e11febfba352e4dc1
Jeb Terry <jbterry77@yahoo.com>
2013-04-05 10:10:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jebapp::Application.config.secret_token = 'c2098b6ff278ffe44f755976f3163b35c3ddbd2bb3da430f0c86c6085272098ee0eb4dd1ce181d5893059a63e2ac3ff0324d930888b5964f5639c404c3760787'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jebapp::Application.config.session_store :cookie_store, key: '_jebapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jebapp::Application.config.session_store :active_record_store
